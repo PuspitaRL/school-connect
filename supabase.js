eb8f779b48c5a508f16b8d9ed46f85915c994c04
PuspitaRL <50951531+PuspitaRL@users.noreply.github.com>
2026-09-12 11:07:22 +0700

    Delete supabase.js

--- supabase.js
+++
@@ -1,9 +0,0 @@
-const SUPABASE_URL = 'https://jmqqkakursfhbpyzpbwl.supabase.co';
-
-const SUPABASE_KEY = 'sb_publishable_x78NaYPauTN7OcKtXqfytg_Piz2Go2Q';
-
-const supabaseClient = window.supabase.createClient(
-    SUPABASE_URL,
-    SUPABASE_KEY
-);
-console.log('Supabase siap:', supabaseClient);
